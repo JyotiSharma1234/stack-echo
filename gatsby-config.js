debcd90b0c0dba159eda70991ee5655d1dc202c3
JYOTI SHARMA <jyoti.sharma@promobitech.com>
2021-06-13 22:48:37 +0530

    header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Stack Echo`,
+    description: `A simple game to test your memorising power. `,
+    author: `@jyotiSharma`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,73 @@
-import * as React from "react"
+import React from 'react'
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { Modal, List } from 'antd';
+import 'antd/dist/antd.css';
+import { QuestionCircleOutlined, RightCircleTwoTone } from '@ant-design/icons';
+import './customCss/header.css';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+    function info() {
+        let data = ['Random numbers( 0-9 ) will be shown on the screen one at a time.',
+                    'Memorise the sequence of the number generated from the beginning of the game.',
+                    'Click the numbers in the sequence right from the first number.'
+                ]
+    Modal.info({
+        title: 'How to Play?',
+        content: (
+            <div>
+                <List
+                size="small"
+                itemLayout="horizontal"
+                dataSource={data}
+                renderItem={item => (
+                    <List.Item>
+                        <List.Item.Meta
+                        avatar={<RightCircleTwoTone className="list-icon" twoToneColor="rgb(32, 230, 183)"/>}
+                        title={item}
+                        />
+                    </List.Item>
+                    )}
+                />,
+            </div>
+        ),
+        onOk() {},
+        });
+    }
+
+    return (
+        <>
+            <header
+            style={{
+                background: `#20e6b7`,
+                marginBottom: `1.45rem`,
+            }}>
+                <div
+                    style={{
+                        margin: `0 auto`,
+                        maxWidth: 960,
+                        padding: `1.45rem 1.0875rem`,
+                        display: 'flex',
+                        justifyContent: 'space-between'
+                    }}>
+                    <h1 style={{ margin: 0 }}>
+                        <Link
+                        to="/"
+                        style={{
+                            color: `white`,
+                            textDecoration: `none`,
+                        }}
+                        >
+                        {siteTitle}
+                        </Link>
+                    </h1>
+
+                    <QuestionCircleOutlined className="help-icon" onClick={info}/>
+                </div>
+            </header>
+      </>
+    )
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
